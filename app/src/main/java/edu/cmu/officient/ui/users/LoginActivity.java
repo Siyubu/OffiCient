273c5a342e6590c0e7d5ee4e8766c9913aba5fe5
Wuyeh Jobe <jwuyeh@gmail.com>
2019-12-01 13:08:33 +0200

    Login activity modified

--- app/src/main/java/edu/cmu/officient/ui/users/LoginActivity.java
+++ app/src/main/java/edu/cmu/officient/ui/users/LoginActivity.java
@@ -56,7 +56,7 @@ public class LoginActivity extends AppCompatActivity {
         registrationOpener.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                Intent intent = new Intent(LoginActivity.this, AddCourseActivity.class);
                 startActivity(intent);
             }
         });
